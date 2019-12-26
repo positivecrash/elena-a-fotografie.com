aa5849ae933eb7f0ca029f03fff731d754ba0062
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-12-26 17:55:06 +0400

    some result

--- dist/assets/js/anisimova_plugins.min.js
+++ dist/assets/js/anisimova_plugins.min.js
@@ -1,32 +1,32 @@
 
-// function Tabs() {
-//   var bindAll = function() {
-//     var menuElements = document.querySelectorAll('[data-tab]');
-//     console.log(menuElements.length);
-//     for(var i = 0; i < menuElements.length ; i++) {
-//       menuElements[i].addEventListener('click', change, false);
-//     }
-//   }
+function Tabs() {
+  var bindAll = function() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    console.log(menuElements.length);
+    for(var i = 0; i < menuElements.length ; i++) {
+      menuElements[i].addEventListener('click', change, false);
+    }
+  }
 
-//   var clear = function() {
-//     var menuElements = document.querySelectorAll('[data-tab]');
-//     for(var i = 0; i < menuElements.length ; i++) {
-//       menuElements[i].classList.remove('active');
-//       var id = menuElements[i].getAttribute('data-tab');
-//       document.getElementById(id).classList.remove('active');
-//     }
-//   }
+  var clear = function() {
+    var menuElements = document.querySelectorAll('[data-tab]');
+    for(var i = 0; i < menuElements.length ; i++) {
+      menuElements[i].classList.remove('active');
+      var id = menuElements[i].getAttribute('data-tab');
+      document.getElementById(id).classList.remove('active');
+    }
+  }
 
-//   var change = function(e) {
-//     clear();
-//     e.target.classList.add('active');
-//     var id = e.currentTarget.getAttribute('data-tab');
-//     document.getElementById(id).classList.add('active');
-//   }
+  var change = function(e) {
+    clear();
+    e.target.classList.add('active');
+    var id = e.currentTarget.getAttribute('data-tab');
+    document.getElementById(id).classList.add('active');
+  }
 
-//   bindAll();
-// }
+  bindAll();
+}
 
-// window.addEventListener('load', function(){
-//   var connectTabs = new Tabs();
-// }, false);
+window.addEventListener('load', function(){
+  var connectTabs = new Tabs();
+}, false);
