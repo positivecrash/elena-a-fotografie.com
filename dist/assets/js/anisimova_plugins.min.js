025e1472669a71371803dfb4d0e6dab6c83a3a80
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2019-12-26 22:32:25 +0400

    mobile navigation new

--- dist/assets/js/anisimova_plugins.min.js
+++ dist/assets/js/anisimova_plugins.min.js
@@ -1,4 +1,45 @@
+window.addEventListener('load', function(){
 
+	var menubtn = document.querySelector('#nav-mobile_toggle'),
+		popup = document.querySelector('#nav-mobile'),
+		closebtn = document.querySelector('#nav-mobile--close'),
+		class_open = 'open';
+
+
+
+	function toggle(element){
+
+		if ( element ){
+	
+			element.addEventListener('click', function(event) {
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				if (popup.classList) { 
+					popup.classList.toggle(class_open);
+				}
+
+			});
+
+		}	
+
+	}
+
+
+	
+
+	if( menubtn ){
+		toggle(menubtn);
+	}
+
+
+	if( closebtn ){
+		toggle(closebtn);
+	}
+	
+
+}, false);
 function Tabs() {
   var bindAll = function() {
     var menuElements = document.querySelectorAll('[data-tab]');
